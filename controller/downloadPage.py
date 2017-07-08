bf57c6ec90a6c80b29a87502f64bb4bf05d80c95
Vinícius Madureira <ViniciusMadureira@users.noreply.github.com>
2017-07-07 23:57:30 -0300

    Update downloadPage.py

--- controller/downloadPage.py
+++ controller/downloadPage.py
@@ -1,1 +1,13 @@
+# -*- coding: utf-8 -*-
+__author__ = "Vinícius Silva Madureira Pereira <contato@viniciusmadureira.com>"
+__date__ = "$Jul 7, 2017 8:37:50 PM$"
 
+import sys
+sys.path.insert(0, 'model')
+from download import Download
+
+class DownloadPage:
+    
+    @staticmethod
+    def get_html_content(url, data, headers, method):                
+        return Download.post(url, data, headers) if str(method).lower() == 'post' else Download.get(url)
